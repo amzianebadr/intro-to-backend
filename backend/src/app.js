e309d6435a67c854a0f73786b0e6a0814ffb6442
Badr Amzaine <badramzaine@gmail.com>
2026-01-31 18:05:24 +0100

    feat: add auth APIs(register, login, logout)

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,16 @@
-import express from "express";
+ import express from "express";
 
-const app=express();
+ const app = express();  // create an express app
+ app.use(express.json());
 
+ // routes import
+import userRouter from "./routes/user.route.js";
+ 
 
+// routes declaration 
+app.use("/api/v1/users", userRouter);
+ 
 
-export default app ;
+// example route : http://localhost:4000/api/v1/users/register
+
+ export default app ;
