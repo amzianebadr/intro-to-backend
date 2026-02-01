5914bf0f158b855001b522ed845a5b5841997fe7
Badr Amzaine <badramzaine@gmail.com>
2026-02-01 21:24:44 +0100

    feat : add crud APIs

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,10 +5,12 @@
 
  // routes import
 import userRouter from "./routes/user.route.js";
+import postRouter from "./routes/post.route.js";
  
 
 // routes declaration 
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/posts", postRouter);
  
 
 // example route : http://localhost:4000/api/v1/users/register
